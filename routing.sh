966cb3eaad420014ca949411fcfa16e37009815c
Antonio Javier Rivero Moreno <61736459+antonio-moreno@users.noreply.github.com>
2021-12-16 20:23:40 +0100

    Update routing.sh

--- routing.sh
+++ routing.sh
@@ -3,5 +3,6 @@
 sudo iptables -Z
 sudo iptables -F 
 sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE 
+sudo mv routing.sh /etc/init.d/
 
 #if your script does not work change eth1 to your network card for internet output e.g. ens33 or ens38 
