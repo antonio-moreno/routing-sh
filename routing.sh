2e0a2bd4a32bdcafec679e0bed1a71f8f881da13
Antonio Javier Rivero Moreno <61736459+antonio-moreno@users.noreply.github.com>
2021-12-16 20:23:06 +0100

    Create routing.sh

---
+++ routing.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+sudo iptables -Z
+sudo iptables -F 
+sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE 
+
+#if your script does not work change eth1 to your network card for internet output e.g. ens33 or ens38 
