e96e1caa5ed0e77041891d231cf6162bf5c11e50
Antonio Javier Rivero Moreno <61736459+antonio-moreno@users.noreply.github.com>
2021-12-16 21:04:48 +0100

    Update routing.sh

--- routing.sh
+++ routing.sh
@@ -1,7 +1,14 @@
 #!/bin/bash 
-
+#Antonio Javier Rivero Moreno 
 sudo iptables -Z
 sudo iptables -F 
 sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE 
 sudo mv routing.sh /etc/init.d/
+#rev.V1
+line="@reboot /etc/init.d/routing.sh"
+(crontab -u $USER -l ; echo "$line" ) | crontab -u $USER -
+#rev.V2
+clear 
+echo -e  "\n\t\t\tCOMPLETE !!!!!"
+
 #if your script does not work change eth1 to your network card for internet output e.g. ens33 or ens38 
