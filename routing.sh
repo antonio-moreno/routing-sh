346ee163fc9721a7d6c468598577f629af8d8a94
Antonio Javier Rivero Moreno <61736459+antonio-moreno@users.noreply.github.com>
2022-02-14 16:04:44 +0100

    Update routing.sh

--- routing.sh
+++ routing.sh
@@ -4,10 +4,6 @@ sudo iptables -Z
 sudo iptables -F 
 sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE 
 sudo mv routing.sh /etc/init.d/
-#rev.V1
-line="@reboot /etc/init.d/routing.sh"
-(crontab -u root -l ; echo "$line" ) | crontab -u root -
-#rev.V2
 clear 
 echo -e  "\n\t\t\tCOMPLETE !!!!!"
 
